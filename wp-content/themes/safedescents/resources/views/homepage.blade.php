79c0079ce332f549843fe4405a6c8dd0fe958926
lexinamer <lexinamer@gmail.com>
2018-06-19 09:42:12 -0400

    Begin styling on homepage and global elements of nav, btns, and typography

--- wp-content/themes/safedescents/resources/views/homepage.blade.php
+++ wp-content/themes/safedescents/resources/views/homepage.blade.php
@@ -12,34 +12,41 @@
   </section>
 
   <section class="services">
-    @if(($services))
-      @foreach($services as $service)
-        <article style="background-image:url({!! $service->service_image !!})">
-          <h3>{{ $service->service_title }}</h3>
-          <p>Can cost up to
-            <span>{{ $service->service_price }}</span>
-          </p>
-        </article>
-      @endforeach
-    @endif
+    <div class="container">
+      @if(($services))
+        @foreach($services as $service)
+          <article style="background-image:url({!! $service->service_image !!})">
+            <h3>{{ $service->service_title }}</h3>
+
+            <div>
+              <h5>Can cost up to</h5>
+              <h4>{{ $service->service_price }}</h4>
+            </div>
+          </article>
+        @endforeach
+      @endif
+    </div>
+
+    <a href="/coverage/" class="btn">See Coverage</a>
   </section>
 
-  <a href="/coverage/" class="btn">See Coverage</a>
-
-  <section class="products" style="background-image:url({!! $products->products_image !!})">
+  <section class="products" style="background-image:url({!! $products->product_image !!})">
     <div class="banner">{{$products->product_banner}}</div>
-    {{$products->product_header}}
 
-    @if(($products))
-      @foreach($products->product as $product)
-        <article>
-          <p>Starting at</p>
-          {{$product->product_price}}
-          {{$product->product_description}}
-          <a href="{{$product->product_link}}">Buy Now</a>
-        </article>
-      @endforeach
-    @endif
+    <div class="container">
+      <h3>{{$products->product_header}}</h3>
+
+      @if(($products))
+        @foreach($products->product as $product)
+          <article>
+            <p>Starting at</p>
+            <h2>{{$product->product_price}}</h2>
+            <h5>{{$product->product_description}}</h5>
+            <a class="btn" href="{{$product->product_link}}">Buy Now</a>
+          </article>
+        @endforeach
+      @endif
+    </div>
   </section>
 
   <section class="testimonials">
