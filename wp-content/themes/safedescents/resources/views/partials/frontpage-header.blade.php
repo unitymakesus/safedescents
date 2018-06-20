b414902640882a7eb8fb7f22906193958d9a8613
lexinamer <lexinamer@gmail.com>
2018-06-20 11:49:30 -0400

    Styling homepage elements

--- wp-content/themes/safedescents/resources/views/partials/frontpage-header.blade.php
+++ wp-content/themes/safedescents/resources/views/partials/frontpage-header.blade.php
@@ -1,7 +1,13 @@
 <header>
   <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
 
-  <div class="container header" style="background-image: url('{!! get_the_post_thumbnail_url($id, 'large') !!}">
+  <!-- <div class="container header" style="background-image: url('{!! get_the_post_thumbnail_url($id, 'large') !!}"> -->
+  <div class="container header">
+    <video muted autoplay loop poster="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Mt_Baker.jpg">
+    	<source src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Mt_Baker.webm" type="video/webm">
+    	<source src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Mt_Baker.mp4" type="video/mp4">
+    </video>
+
     <div class="nav-container">
         @if (has_custom_logo())
           @php
