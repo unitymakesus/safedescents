4ca15fec3aff9b353260d9acd88e2a3872c65bb8
Alisa Herr <alisa@unitymakes.us>
2018-07-17 15:39:18 -0400

    Start changing tooltip over from WC to JSON

--- wp-content/themes/safedescents/resources/views/partials/tooltip.blade.php
+++ wp-content/themes/safedescents/resources/views/partials/tooltip.blade.php
@@ -1,54 +1,38 @@
-@if ($products->have_posts())
-  @while ($products->have_posts())
+@php
+  $states_json = file_get_contents(get_template_directory() . '/../app/sdk/api-products.json');
+
+  if (!empty($states_json)) {
+    $states = json_decode($states_json);
+  }
+
+  // if ($pass_duration = $variation['attributes']['attribute_pa_pass']) {
+  //   $passes[$pass_duration]['price'] = $variation['display_price'];
+  //   $passes[$pass_duration]['variation_id'] = $variation['variation_id'];
+  //   $passes[$pass_duration]['attributes'] = $variation['attributes'];
+  // }
+@endphp
+@if (!empty($states))
+  @foreach ($states as $state)
     @php
-      // WP post object
-      $products->the_post();
-      // WC product object
-      global $product;
-      $product_id = $product->get_id();
+      var_dump($state);
     @endphp
 
     <div class="tooltip" id="{{the_title()}}">
       <div class="state">{{ the_title() }}</div>
-      @if ($product->is_type('variable'))
-        @php
-          $passes = array(
-            'season-pass' => array(
-              'label' => 'Per Season'
-            ),
-            'daily-pass' => array(
-              'label' => 'Per Day'
-            )
-          );
-
-          $variations = $product->get_available_variations();
-          foreach ($variations as $variation) {
-            if ($pass_duration = $variation['attributes']['attribute_pa_pass']) {
-              $passes[$pass_duration]['price'] = $variation['display_price'];
-              $passes[$pass_duration]['variation_id'] = $variation['variation_id'];
-              $passes[$pass_duration]['attributes'] = $variation['attributes'];
-            }
-          }
-        @endphp
-
-        @foreach ($passes as $pass)
-          <div class="variation">
-            <div class="price">
-              ${{ $pass['price'] }}
-            </div>
-            <div class="duration">
-              {{ $pass['label'] }}
-            </div>
-            <div class="buy">
-              <a rel="nofollow" href="{!! do_shortcode('[add_to_cart_url id="' . $pass['variation_id'] . '"]') !!}" class="btn">Buy Now</a>
-              {{-- <a ref="nofollow" class="btn" href="/buy-now#purchase">Buy Now</a> --}}
-            </div>
-          </div>
-        @endforeach
-
-      @endif
+      <div class="variation">
+        <div class="price">
+          ${{ $pass['price'] }}
+        </div>
+        <div class="duration">
+          {{ $pass['label'] }}
+        </div>
+        <div class="buy">
+          <a rel="nofollow" href="{!! do_shortcode('[add_to_cart_url id="' . $pass['variation_id'] . '"]') !!}" class="btn">Buy Now</a>
+          {{-- <a ref="nofollow" class="btn" href="/buy-now#purchase">Buy Now</a> --}}
+        </div>
+      </div>
     </div>
-  @endwhile
+  @endforeach
 @endif
 
 @php (wp_reset_postdata())
