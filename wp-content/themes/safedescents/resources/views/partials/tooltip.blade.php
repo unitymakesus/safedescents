5b7b343c4f312499d789624a4bb39b564f213711
lexinamer <lexinamer@gmail.com>
2018-08-01 15:59:16 -0400

    redoing and finishing the coverage page and making it resopnsive

--- wp-content/themes/safedescents/resources/views/partials/tooltip.blade.php
+++ wp-content/themes/safedescents/resources/views/partials/tooltip.blade.php
@@ -4,22 +4,71 @@
   if (!empty($states_json)) {
     $states = json_decode($states_json);
   }
-
-  // if ($pass_duration = $variation['attributes']['attribute_pa_pass']) {
-  //   $passes[$pass_duration]['price'] = $variation['display_price'];
-  //   $passes[$pass_duration]['variation_id'] = $variation['variation_id'];
-  //   $passes[$pass_duration]['attributes'] = $variation['attributes'];
-  // }
 @endphp
+
+  <div class="coverage-details placeholder" data-state="{{ $state->location }}">
+    Select your state to check availability.
+  </div>
+
+  <div class="coverage-mobile-select">
+    <select required name="coverage_select" id="coverage_select">
+      <option value="">Select an option…</option>
+      <option value="AL">Alabama</option>
+      <option value="AZ">Arizona</option>
+      <option value="AR">Arkansas</option>
+      <option value="CA">California</option>
+      <option value="CO">Colorado</option>
+      <option value="CT">Connecticut</option>
+      <option value="DE">Delaware</option>
+      <option value="DC">District Of Columbia</option>
+      <option value="FL">Florida</option>
+      <option value="GA">Georgia</option>
+      <option value="ID">Idaho</option>
+      <option value="IL">Illinois</option>
+      <option value="IN">Indiana</option>
+      <option value="IA">Iowa</option>
+      <option value="KS">Kansas</option>
+      <option value="KY">Kentucky</option>
+      <option value="LA">Louisiana</option>
+      <option value="ME">Maine</option>
+      <option value="MD">Maryland</option>
+      <option value="MA">Massachusetts</option>
+      <option value="MI">Michigan</option>
+      <option value="MN">Minnesota</option>
+      <option value="MS">Mississippi</option>
+      <option value="MO">Missouri</option>
+      <option value="MT">Montana</option>
+      <option value="NE">Nebraska</option>
+      <option value="NV">Nevada</option>
+      <option value="NH">New Hampshire</option>
+      <option value="NJ">New Jersey</option>
+      <option value="NM">New Mexico</option>
+      <option value="NY">New York</option>
+      <option value="NC">North Carolina</option>
+      <option value="ND">North Dakota</option>
+      <option value="OH">Ohio</option>
+      <option value="OK">Oklahoma</option>
+      <option value="OR">Oregon</option>
+      <option value="PA">Pennsylvania</option>
+      <option value="RI">Rhode Island</option>
+      <option value="SC">South Carolina</option>
+      <option value="SD">South Dakota</option>
+      <option value="TN">Tennessee</option>
+      <option value="TX">Texas</option>
+      <option value="UT">Utah</option>
+      <option value="VT">Vermont</option>
+      <option value="VA">Virginia</option>
+      <option value="WA">Washington</option>
+      <option value="WV">West Virginia</option>
+      <option value="WI">Wisconsin</option>
+      <option value="WY">Wyoming</option>
+    </select>
+  </div>
+
 @if (!empty($states))
   @foreach ($states as $state)
-    @php
-      //var_dump($state);
-    @endphp
-
-    <div class="available" data-state="{{ $state->location }}">
+    <div class="coverage-details coverage-state available" data-state="{{ $state->location }}">
       <p class="state-name">{{ $state->location }}</p>
-
       @foreach ($state->variations as $variation)
         <div class="variation">
           <div class="duration">
@@ -38,15 +87,14 @@
           <a ref="nofollow" class="btn" href="/buy-now/?configuration_id={{ $variation->configuration_id }}">Buy Now</a>
         </div>
       @endforeach
-
     </div>
   @endforeach
 @endif
 
 @php (wp_reset_postdata())
 
-<div class="tooltip not-available">
+<div class="coverage-details coverage-state not-available">
   <p class="state-name">Not Yet Available</p>
-  <p>Please enter your email below to be notified as soon as Safe Descents is available in your state.</p>
+  <p>Enter your email below to find out when Safe Descents in available in your area.</p>
   {!! do_shortcode('[contact-form-7 id="377" title="State Interest Form"]') !!}
 </div>
