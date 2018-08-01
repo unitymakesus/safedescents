19e5d332a7cacf1d5d355e68e70d8d9e81525d8a
Alisa Herr <alisa@unitymakes.us>
2018-08-01 17:18:40 -0400

    Fix larger screens for home

--- wp-content/themes/safedescents/resources/views/partials/footer.blade.php
+++ wp-content/themes/safedescents/resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <img class="footer-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/skilift.jpeg"/>
 
     <div class="row">
-      <div class="col-xs col-sm-4 col-md-3">
+      <div class="col-xs col-sm-4 col-md-3 col-lg-2">
         <a class="logo" href="{{ home_url('/') }}" rel="home">
           @if (has_custom_logo())
             @php
@@ -20,22 +20,20 @@
         </a>
       </div>
 
-      <div class="col-xs-8 col-md-6 footer-menu">
+      <div class="col-xs-8 col-md-6 col-lg-5 footer-menu">
         @php dynamic_sidebar('sidebar-footer') @endphp
       </div>
 
-      <div class="col-sm col-md-3">
+      <div class="col-sm col-md-3 last">
         @php dynamic_sidebar('sidebar-footer-2') @endphp
       </div>
     </div>
 
     <div class="footer-copyright">
-      <div class="wrapper">
-        <div class="flex flex-center space-between">
-          <span class="copyright">&copy; @php(current_time('Y')) Safe Descents &mdash; All products are underwritten by Starr Indemnity & Liability Company</span>
-           | <a class="privacy" href="<?php get_site_url()?>/privacy-policy/">Privacy Policy</a>
-          @include('partials.unity')
-        </div>
+      <div class="flex flex-center space-between">
+        <span class="copyright">&copy; @php(current_time('Y')) Safe Descents &mdash; All products are underwritten by Starr Indemnity & Liability Company</span>
+         | <a class="privacy" href="<?php get_site_url()?>/privacy-policy/">Privacy Policy</a>
+        @include('partials.unity')
       </div>
     </div>
   </div>
