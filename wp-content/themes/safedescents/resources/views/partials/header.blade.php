9afd37416f39f89cefb6f7aaf0e14e5994e9d034
lexinamer <lexinamer@gmail.com>
2018-08-06 11:24:03 -0400

    editing images and small details for the checkout and home pages

--- wp-content/themes/safedescents/resources/views/partials/header.blade.php
+++ wp-content/themes/safedescents/resources/views/partials/header.blade.php
@@ -2,13 +2,13 @@
   <header class="header-frontpage">
 
 @elseif(is_single() || is_category("testimonials"))
-  <header style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/images/mountains.jpeg')">
+  <header style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/images/snowcaps.jpeg')">
 
 @else
   @if(has_post_thumbnail())
     <header style="background-image: url('{!! get_the_post_thumbnail_url($id, 'full') !!}')">
   @else
-    <header style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/images/mountains.jpeg')">
+    <header style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/images/snowcaps.jpeg')">
   @endif
 @endif
 
