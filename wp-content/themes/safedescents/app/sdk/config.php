7a85fa801ebc2741563f94d9fa4b6fef37c35ec1
Alisa Herr <alisa@unitymakes.us>
2018-08-24 13:05:12 -0400

    Api key changed

--- wp-content/themes/safedescents/app/sdk/config.php
+++ wp-content/themes/safedescents/app/sdk/config.php
@@ -2,6 +2,6 @@
 
 return [
 	'access_id' => "02a9b219-44cd-47a7-8580-089c5603e9a1",
-	'api_key' => "fvXG1inOhI6YBvRNTiHE7cLNCt69XfSaRVK5xmIQB08lIRy7YVnLguT4JVJhkVVB5sgx7GOpVIu81MmBQfDIryd5BXW3HD5rtWLjcjlLv15ZcHGzwn0Cedp2RJt0BW03",
+	'api_key' => "sovvUeYwghqXZBu1yXGHi09rx4cnnchkkeCXu2znu2LDiD7ZBfQIUAkfxBhc4dO2HtIFQ4vsSk61jlhoYbCZ4bnZoVquhgg5QgP3NIJWkU7f2NXtzItVCq5dcg19gKDr",
 	'domain' => "https://demo.api.safedescents.com/api/v1"
 ];
