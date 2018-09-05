29d9a6eb4cf8c77d14497a2138ee995e3d2ddd15
Alisa Herr <alisa@unitymakes.us>
2018-09-05 15:27:09 -0400

    Update plugins

--- wp-content/plugins/advanced-custom-fields-pro/includes/api/api-term.php
+++ wp-content/plugins/advanced-custom-fields-pro/includes/api/api-term.php
@@ -79,9 +79,9 @@ function acf_get_taxonomy_labels( $taxonomies = array() ) {
 	}
 	
 	// show taxonomy name next to label for shared labels
-	foreach( $ref as $label => $count ) {
-		if( $count > 1 ) {
-			$data[ $label ] .= ' (' . $count . ')';
+	foreach( $data as $taxonomy => $label ) {
+		if( $ref[$label] > 1 ) {
+			$data[ $taxonomy ] .= ' (' . $taxonomy . ')';
 		}
 	}
 	
@@ -231,6 +231,69 @@ function _acf_terms_clauses( $pieces, $taxonomies, $args ) {
 
 function acf_get_pretty_taxonomies( $taxonomies = array() ) {
 	return acf_get_taxonomy_labels( $taxonomies );
+}
+
+/**
+*  acf_get_term
+*
+*  Similar to get_term() but with some extra functionality.
+*
+*  @date	19/8/18
+*  @since	5.7.3
+*
+*  @param	mixed $term_id The term ID or a string of "taxonomy:slug".
+*  @param	string $taxonomy The taxonomyname.
+*  @return	WP_Term
+*/
+
+function acf_get_term( $term_id, $taxonomy = '' ) {
+	
+	// allow $term_id parameter to be a string of "taxonomy:slug" or "taxonomy:id"
+	if( is_string($term_id) && strpos($term_id, ':') ) {
+		list( $taxonomy, $term_id ) = explode(':', $term_id);
+		$term = get_term_by( 'slug', $term_id, $taxonomy );
+		if( $term ) return $term;
+	}
+	
+	// return
+	return get_term( $term_id, $taxonomy );
+}
+
+/**
+*  acf_encode_term
+*
+*  Returns a "taxonomy:slug" string for a given WP_Term.
+*
+*  @date	27/8/18
+*  @since	5.7.4
+*
+*  @param	WP_Term $term The term object.
+*  @return	string
+*/
+function acf_encode_term( $term ) {
+	return "{$term->taxonomy}:{$term->slug}";
+}
+
+/**
+*  acf_decode_term
+*
+*  Decodes a "taxonomy:slug" string into an array of taxonomy and slug.
+*
+*  @date	27/8/18
+*  @since	5.7.4
+*
+*  @param	WP_Term $term The term object.
+*  @return	string
+*/
+function acf_decode_term( $string ) {
+	if( is_string($string) && strpos($string, ':') ) {
+		list( $taxonomy, $slug ) = explode(':', $string);
+		return array(
+			'taxonomy'	=> $taxonomy,
+			'slug'		=> $slug
+		);
+	}
+	return false;
 }
 
 ?>
--- wp-content/plugins/advanced-custom-fields-pro/readme.txt
+++ wp-content/plugins/advanced-custom-fields-pro/readme.txt
@@ -66,6 +66,27 @@ From your WordPress dashboard
 
 == Changelog ==
 
+= 5.7.4 =
+*Release Date - 30 August 2018*
+
+* Fix - Fixed bug causing field groups to disappear when selecting a taxonomy term with WPML active.
+* Tweak - Added more Dark Mode styles.
+* Tweak - Improved DB upgrade prompt, functions and AJAX logic.
+* Tweak - Improved the "What's New" admin page seen after DB Upgrade.
+* Dev - Added new location rules filters.
+
+= 5.7.3 =
+*Release Date - 20 August 2018*
+
+* New - Added Dark Mode styles for the [Dark Mode Plugin](https://en-au.wordpress.org/plugins/dark-mode/).
+* New - Added "Value Contains" condition to the Select field type.
+* New - Added support for the WooCommerce product type dropdown to trigger "update metaboxes".
+* Tweak - Improved acf.screen model responsible for "updating metaboxes" when changing post data.
+* Tweak - Removed user fields from the multisite "Add New User" page. 
+* Fix - Fixed bug preventing some tinymce customizations from working.
+* Fix - Fixed JS bug throwing "preference" error in console.
+* Dev - Added action 'acf/enqueue_uploader' triggered after the hidden "ACF Content" editor is rendered.
+
 = 5.7.2 =
 *Release Date - 6 August 2018*
 
